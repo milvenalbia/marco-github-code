246b65ba0b0d462a212ea5c9562b43fbbf23ce69
Milven <albiav.007@gmail.com>
2024-09-13 19:56:12 +0800

    Hash Router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
         <div className="flex items-center gap-2">
           <img className="w-12" src={Logo} alt="" />
           <span className="text-3xl font-bold text-indigo-900 dark:text-white">
-            Portwind.
+            Marco.
           </span>
         </div>
         {/* Menu */}
